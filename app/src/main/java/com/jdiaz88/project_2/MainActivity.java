79941fa76f3f64fcdaca488fb3c3031e530bdc42
Jacob Diaz <hello@jacobdiaz.dev>
2021-02-15 17:46:54 -0600

    first Commit

---
+++ app/src/main/java/com/jdiaz88/project_2/MainActivity.java
@@ -0,0 +1,13 @@
+package com.jdiaz88.project_2;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        getSupportActionBar().hide(); // Hides the title bar
+    }
+}
