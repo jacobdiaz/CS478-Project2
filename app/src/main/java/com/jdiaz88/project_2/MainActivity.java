6dffded25e45e7c3de1bb465fd4a0efa934be46a
Jacob Diaz <hello@jacobdiaz.dev>
2021-02-24 18:29:37 -0600

    created working on click to new youtube activity

--- app/src/main/java/com/jdiaz88/project_2/MainActivity.java
+++ app/src/main/java/com/jdiaz88/project_2/MainActivity.java
@@ -50,32 +50,31 @@ public class MainActivity extends AppCompatActivity implements SongAdapter.songC
         songRecyclerView.setAdapter(sAdapter);
     }
 
-    // TODO change the shape of Song object so it doesn't use album cover path
     private void generateSongList(){
-        songList.add(new Song("Power","Kanye West","","#"));
-        songList.add(new Song("Chanel","Frank Ocean","","#"));
-        songList.add(new Song("10 Years Ago","FKJ","","#"));
-        songList.add(new Song("Are You Bored Yet?(feat. Clairo)","Wallows,Clairo","","#"));
-        songList.add(new Song("After the Storm","Tyler the Creator, Kali Uchis","","#"));
-        songList.add(new Song("Photosynthesis","Saba, Jean Deaux","","#"));
-        songList.add(new Song("CYANIDE","Daniel Caesar","","#"));
-        songList.add(new Song("The Spins","Mac Miller","","#"));
-        songList.add(new Song("Chronic Sunshine","Cosmo Pyke","","#"));
+        songList.add(new Song("Power","Kanye West","https://www.youtube.com/watch?v=SUtf9Ajlno4"));
+        songList.add(new Song("Chanel","Frank Ocean","https://www.youtube.com/watch?v=MRyNOYrt6Wc"));
+        songList.add(new Song("10 Years Ago","FKJ","https://www.youtube.com/watch?v=rqScfATfNnc"));
+        songList.add(new Song("Are You Bored Yet?(feat. Clairo)","Wallows,Clairo","https://www.youtube.com/watch?v=wjbAsm48oTA"));
+        songList.add(new Song("After the Storm","Tyler the Creator, Kali Uchis","https://www.youtube.com/watch?v=6VwC2P_gYGg"));
+        songList.add(new Song("Photosynthesis","Saba, Jean Deaux","https://www.youtube.com/watch?v=3Oi13wz1KlA"));
+        songList.add(new Song("CYANIDE","Daniel Caesar","https://www.youtube.com/watch?v=09J5ttP2YDM"));
+        songList.add(new Song("The Spins","Mac Miller","https://www.youtube.com/watch?v=mkGT1c98soU"));
+        songList.add(new Song("Chronic Sunshine","Cosmo Pyke","https://www.youtube.com/watch?v=iOSSAQPt-Ro"));
     }
 
     @Override
     public void onSongClicked(int position) {
         final Song clickedSong = songList.get(position);
-        Log.d("clickedSong","Clicked "+clickedSong.getSongTitle()+" ");
-        // Navigate to a new activity
-//        Intent i = new Intent(this,newactivity.class)
+        Log.d("clickedSong","Clicked "+clickedSong.getSongUrl()+" ");
+//        // Navigate to a new activity
+//        Intent i = new Intent(Intent.ACTION_VIEW);
+//        i.setData((Uri.parse(clickedSong.getSongUrl())));
+//        startActivity(i);
+        Uri uri = Uri.parse(clickedSong.getSongUrl());
+        startActivity(new Intent(Intent.ACTION_VIEW,uri));
+
     }
 }
-
-
-
-
-
 // Song Image Resources
 //https://www.google.com/url?sa=i&url=https%3A%2F%2Fgenius.com%2FDaniel-caesar-cyanide-lyrics&psig=AOvVaw2rLIjTfugsWJRuUwd9aYDr&ust=1614294954885000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCLjMpL3Tg-8CFQAAAAAdAAAAABAD
 //https://www.google.com/url?sa=i&url=https%3A%2F%2Fen.wikipedia.org%2Fwiki%2FK.I.D.S._(mixtape)&psig=AOvVaw2BK0qI_HdwBkz-iCECXrVh&ust=1614295165978000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCLDNtaLUg-8CFQAAAAAdAAAAABAD
--- app/src/main/java/com/jdiaz88/project_2/Song.java
+++ app/src/main/java/com/jdiaz88/project_2/Song.java
@@ -3,20 +3,15 @@ package com.jdiaz88.project_2;
 public class Song {
     private String songTitle;
     private String artistName;
-    private String albumCoverPath;
     private String songUrl;
 
 
-    public Song(String songTitle, String artistName, String albumCoverPath,String songUrl){
+    public Song(String songTitle, String artistName,String songUrl){
         this.songTitle = songTitle;
         this.artistName = artistName;
-        this.albumCoverPath = albumCoverPath;
         this.songUrl = songUrl;
     }
 
-    public String getAlbumCoverPath() {
-        return albumCoverPath;
-    }
 
     public String getArtistName() {
         return artistName;
@@ -30,19 +25,4 @@ public class Song {
         return songUrl;
     }
 
-    public void setAlbumCoverPath(String albumCoverPath) {
-        this.albumCoverPath = albumCoverPath;
-    }
-
-    public void setArtistName(String artistName) {
-        this.artistName = artistName;
-    }
-
-    public void setSongTitle(String songTitle) {
-        this.songTitle = songTitle;
-    }
-
-    public void setSongUrl(String songUrl) {
-        this.songUrl = songUrl;
-    }
 }
