d0a67f436adb7943033ab6b623768ebd7345a63e
Jacob Diaz <hello@jacobdiaz.dev>
2021-02-25 22:26:47 -0600

    GOing back

--- app/src/main/java/com/jdiaz88/project_2/MainActivity.java
+++ app/src/main/java/com/jdiaz88/project_2/MainActivity.java
@@ -11,10 +11,20 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
+<<<<<<< HEAD
+<<<<<<< HEAD
+import android.widget.TextView;
+import android.widget.Toast;
+=======
+import android.view.View;
+>>>>>>> parent of 2f48a95... Added wiki url to song object
+=======
+import android.view.View;
+>>>>>>> parent of 2f48a95... Added wiki url to song object
 
 import java.util.ArrayList;
 
-public class MainActivity extends AppCompatActivity implements SongAdapter.songClickedListener {
+public class MainActivity extends AppCompatActivity {
     private ArrayList<Song> songList;
     private RecyclerView songRecyclerView;
 
@@ -32,11 +42,13 @@ public class MainActivity extends AppCompatActivity implements SongAdapter.songC
 
         // Setup adapter for song list
         setupAdapter();
+
+        //F
     }
 
     private void setupAdapter(){
         // Instantiate adapter
-        SongAdapter sAdapter = new SongAdapter(songList,this::onSongClicked);
+        SongAdapter sAdapter = new SongAdapter(songList);
 
         // Set up layout manager
         RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getApplicationContext());
@@ -50,29 +62,32 @@ public class MainActivity extends AppCompatActivity implements SongAdapter.songC
     }
 
     private void generateSongList(){
-        songList.add(new Song("Power","Kanye West","https://www.youtube.com/watch?v=SUtf9Ajlno4","https://en.wikipedia.org/wiki/Power_(Kanye_West_song)"));
-        songList.add(new Song("Chanel","Frank Ocean","https://www.youtube.com/watch?v=MRyNOYrt6Wc","https://en.wikipedia.org/wiki/Chanel_(Frank_Ocean_song)"));
-        songList.add(new Song("10 Years Ago","FKJ","https://www.youtube.com/watch?v=rqScfATfNnc",""));
-        songList.add(new Song("Are You Bored Yet?(feat. Clairo)","Wallows,Clairo","https://www.youtube.com/watch?v=wjbAsm48oTA","https://en.wikipedia.org/wiki/Wallows"));
-        songList.add(new Song("After the Storm","Tyler the Creator, Kali Uchis","https://www.youtube.com/watch?v=6VwC2P_gYGg","https://en.wikipedia.org/wiki/After_the_Storm_(Kali_Uchis_song)"));
-        songList.add(new Song("Photosynthesis","Saba, Jean Deaux","https://www.youtube.com/watch?v=3Oi13wz1KlA","https://en.wikipedia.org/wiki/Photosynthesis_saba"));
-        songList.add(new Song("CYANIDE","Daniel Caesar","https://www.youtube.com/watch?v=09J5ttP2YDM","https://en.wikipedia.org/wiki/Case_Study_01"));
-        songList.add(new Song("The Spins","Mac Miller","https://www.youtube.com/watch?v=mkGT1c98soU","https://en.wikipedia.org/wiki/Mac_Miller"));
-        songList.add(new Song("Chronic Sunshine","Cosmo Pyke","https://www.youtube.com/watch?v=iOSSAQPt-Ro","https://en.wikipedia.org/wiki/Cosmo_Pyke"));
+        songList.add(new Song("Power","Kanye West","https://www.youtube.com/watch?v=SUtf9Ajlno4"));
+        songList.add(new Song("Chanel","Frank Ocean","https://www.youtube.com/watch?v=MRyNOYrt6Wc"));
+        songList.add(new Song("10 Years Ago","FKJ","https://www.youtube.com/watch?v=rqScfATfNnc"));
+        songList.add(new Song("Are You Bored Yet?(feat. Clairo)","Wallows,Clairo","https://www.youtube.com/watch?v=wjbAsm48oTA"));
+        songList.add(new Song("After the Storm","Tyler the Creator, Kali Uchis","https://www.youtube.com/watch?v=6VwC2P_gYGg"));
+        songList.add(new Song("Photosynthesis","Saba, Jean Deaux","https://www.youtube.com/watch?v=3Oi13wz1KlA"));
+        songList.add(new Song("CYANIDE","Daniel Caesar","https://www.youtube.com/watch?v=09J5ttP2YDM"));
+        songList.add(new Song("The Spins","Mac Miller","https://www.youtube.com/watch?v=mkGT1c98soU"));
+        songList.add(new Song("Chronic Sunshine","Cosmo Pyke","https://www.youtube.com/watch?v=iOSSAQPt-Ro"));
     }
+<<<<<<< HEAD
+=======
 
     @Override
     public void onSongClicked(int position) {
         final Song clickedSong = songList.get(position);
-        Log.d("clickedSong","Clicked "+clickedSong.getYouTubeURL()+" ");
+        Log.d("clickedSong","Clicked "+clickedSong.getSongUrl()+" ");
 //        // Navigate to a new activity
 //        Intent i = new Intent(Intent.ACTION_VIEW);
 //        i.setData((Uri.parse(clickedSong.getSongUrl())));
 //        startActivity(i);
-        Uri uri = Uri.parse(clickedSong.getYouTubeURL());
+        Uri uri = Uri.parse(clickedSong.getSongUrl());
         startActivity(new Intent(Intent.ACTION_VIEW,uri));
 
     }
+>>>>>>> parent of 2f48a95... Added wiki url to song object
 }
 // Song Image Resources
 //https://www.google.com/url?sa=i&url=https%3A%2F%2Fgenius.com%2FDaniel-caesar-cyanide-lyrics&psig=AOvVaw2rLIjTfugsWJRuUwd9aYDr&ust=1614294954885000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCLjMpL3Tg-8CFQAAAAAdAAAAABAD
--- app/src/main/java/com/jdiaz88/project_2/SongAdapter.java
+++ app/src/main/java/com/jdiaz88/project_2/SongAdapter.java
@@ -4,6 +4,8 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
+
 import java.util.ArrayList;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
@@ -15,38 +17,32 @@ public class SongAdapter extends RecyclerView.Adapter<SongAdapter.MyViewHolder> 
     // index of images array corresponds to order/position it is explicitly generated in MainActivity
     private int[] images = { R.drawable.power, R.drawable.chanel, R.drawable.ten, R.drawable.areyouboredyet, R.drawable.afterthestorm,R.drawable.photo,R.drawable.cyanide, R.drawable.thespins, R.drawable.chronicsunshine};
 
-    private songClickedListener scListener;
-
-    public SongAdapter(ArrayList<Song> songList ,songClickedListener scListener){
+    public SongAdapter(ArrayList<Song> songList ){
         this.songList = songList;
-        this.scListener = scListener;
     }
 
     // Create a viewholder
-    public class MyViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
+    public class MyViewHolder extends RecyclerView.ViewHolder{
         private TextView songTitle;
         private TextView artistName;
         private ImageView albumCover;
 
-        songClickedListener songListener;
 
-
-        public MyViewHolder(final View view, songClickedListener songListener){
+        public MyViewHolder(final View view){
             super(view);
             // Linking the UI elements from song_item.xml
             songTitle = view.findViewById(R.id.song_title);
             artistName = view.findViewById(R.id.song_artist);
             albumCover = view.findViewById(R.id.album_cover);
 
-            // Set the listener
-            this.songListener = songListener;
-
-            itemView.setOnClickListener(this);
-        }
-
-        @Override
-        public void onClick(View v) {
-            songListener.onSongClicked(getAdapterPosition());
+            // On long click
+            itemView.setOnLongClickListener(new View.OnLongClickListener() {
+                @Override
+                public boolean onLongClick(View v) {
+                    Toast.makeText(v.getContext(), "Position is" + getAdapterPosition(),Toast.LENGTH_SHORT).show();
+                    return false;
+                }
+            });
         }
     }
 
@@ -56,7 +52,7 @@ public class SongAdapter extends RecyclerView.Adapter<SongAdapter.MyViewHolder> 
     public SongAdapter.MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         // Inflate our view
         View itemView = LayoutInflater.from(parent.getContext()).inflate(R.layout.song_item, parent, false);
-        return new MyViewHolder(itemView, scListener);
+        return new MyViewHolder(itemView);
     }
 
     @Override
@@ -74,10 +70,5 @@ public class SongAdapter extends RecyclerView.Adapter<SongAdapter.MyViewHolder> 
     @Override
     public int getItemCount() {
         return songList.size();
-    }
-
-
-    public interface songClickedListener{
-        void onSongClicked(int position);
     }
 }
